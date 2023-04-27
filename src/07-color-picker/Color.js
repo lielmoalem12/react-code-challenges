d2206d07a4b1f045b545bd26f7ceab76d218a58e
lielmoalem12 <lielmoalem12@gmail.com>
2023-04-27 11:00:28 +0300

    change background color based on button clicks

--- src/07-color-picker/Color.js
+++ src/07-color-picker/Color.js
@@ -1,10 +1,11 @@
-export default function Color ({ hex, name }) {
+export default function Color({ hex, name, setBackgroundColor }) {
   return (
     <button
-      className='color-square'
+      className="color-square"
       style={{ backgroundColor: hex }}
+      onClick={() => setBackgroundColor(hex)}
     >
       <h2>{name}</h2>
     </button>
-  )
+  );
 }
