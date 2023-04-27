ad91c8813cb7976792366cd0676660f15a8f5837
lielmoalem12 <lielmoalem12@gmail.com>
2023-04-27 10:57:33 +0300

    remove WindowEvent on double click

--- src/06-add-window-event/ToggleWindowEvent.js
+++ src/06-add-window-event/ToggleWindowEvent.js
@@ -1,12 +1,28 @@
-import { useState } from 'react'
-import WindowEvent from './WindowEvent'
+import { useEffect, useState } from "react";
+import WindowEvent from "./WindowEvent";
 
-export default function ToggleWindowEvent () {
-  const [windowEvent, setWindowEvent] = useState(false)
+export default function ToggleWindowEvent() {
+  const [windowEvent, setWindowEvent] = useState(false);
+  // add event listener on double click
+
+  useEffect(() => {
+    const handleDoubleClick = () => {
+      setWindowEvent(false);
+    };
+
+    window.addEventListener("dblclick", handleDoubleClick);
+
+    return () => {
+      window.removeEventListener("dblclick", handleDoubleClick);
+    };
+  }, []);
+
   return (
     <div>
-      <button onClick={() => setWindowEvent(prevState => !prevState)}>Toggle Window Event</button>
+      <button onClick={() => setWindowEvent((prevState) => !prevState)}>
+        Toggle Window Event
+      </button>
       {windowEvent && <WindowEvent />}
     </div>
-  )
+  );
 }
