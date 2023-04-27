226990a3fb141f31b58b01be38d9631b551fcbde
lielmoalem12 <lielmoalem12@gmail.com>
2023-04-27 11:11:50 +0300

    create a drawing canvas to practice react context

--- src/08-pixel-art/PixelArt.js
+++ src/08-pixel-art/PixelArt.js
@@ -1,34 +1,72 @@
-import React from 'react'
+import React from "react";
 
-function ColorPicker () {
-  const colors = ['red', 'blue', 'yellow', 'green', 'black', 'white', 'purple']
+function ColorPicker() {
+  const { setSelectedColor } = React.useContext(SelectedColorContext);
+  const colors = ["red", "blue", "yellow", "green", "black", "white", "purple"];
   return (
     <div>
       <h1>Choose a color</h1>
-      {colors.map(color => <button key={color} style={{ backgroundColor: color }} />)}
+      {colors.map((color) => (
+        <button
+          key={color}
+          style={{ backgroundColor: color }}
+          onClick={() => {
+            setSelectedColor(color);
+          }}
+        />
+      ))}
     </div>
-  )
+  );
 }
 
-function Pixel () {
-  return <div style={{ height: '20px', width: '20px', backgroundColor: 'lightGrey', margin: '1px' }} />
-}
-
-function Pixels () {
-  const pixels = []
-  for (let i = 0; i < 100; i++) pixels.push(<Pixel key={i} />)
+function Pixel() {
+  const [color, setColor] = React.useState("lightGrey");
+  const { selectedColor } = React.useContext(SelectedColorContext);
   return (
-    <div style={{ display: 'grid', gridTemplateColumns: 'repeat(10, 1fr)', width: '210px', margin: '0 auto' }}>
+    <div
+      style={{
+        height: "20px",
+        width: "20px",
+        backgroundColor: color,
+        margin: "1px",
+      }}
+      onClick={() => {
+        setColor(selectedColor);
+      }}
+    />
+  );
+}
+
+function Pixels() {
+  const pixels = [];
+  for (let i = 0; i < 100; i++) pixels.push(<Pixel key={i} />);
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(10, 1fr)",
+        width: "210px",
+        margin: "0 auto",
+      }}
+    >
       {pixels}
     </div>
-  )
+  );
 }
+//craete react context for selectedColor
+const SelectedColorContext = React.createContext();
 
-export default function PixelArt () {
+export default function PixelArt() {
+  const [selectedColor, setSelectedColor] = React.useState("");
+
   return (
     <div>
-      <ColorPicker />
-      <Pixels />
+      <SelectedColorContext.Provider
+        value={{ selectedColor, setSelectedColor }}
+      >
+        <ColorPicker />
+        <Pixels />
+      </SelectedColorContext.Provider>
     </div>
-  )
+  );
 }
