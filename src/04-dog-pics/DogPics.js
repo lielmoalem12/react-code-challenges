46cb3937adf0705a86633894b3348a5ba29fdc05
lielmoalem12 <lielmoalem12@gmail.com>
2023-04-27 10:47:46 +0300

    fetch dog images from an api

--- src/04-dog-pics/DogPics.js
+++ src/04-dog-pics/DogPics.js
@@ -1,9 +1,23 @@
-export default function DogPics () {
+import { useEffect, useState } from "react";
+
+export default function DogPics() {
   // API: https://dog.ceo/dog-api/
+  const [dogImg, setDogImg] = useState("");
+
+  useEffect(() => {
+    getDogPic();
+  }, []);
+
+  const getDogPic = async () => {
+    const res = await fetch("https://dog.ceo/api/breeds/image/random");
+    const data = await res.json();
+    setDogImg(data.message);
+  };
+
   return (
-    <div className='dog-pics'>
-      <img src='https://images.dog.ceo/breeds/spaniel-cocker/n02102318_4172.jpg' />
-      <button>🐶</button>
+    <div className="dog-pics">
+      <img src={dogImg} />
+      <button onClick={getDogPic}>🐶</button>
     </div>
-  )
+  );
 }
