bf6f79ec524a1197eb77b9452b8e1005b2efdf9b
lielmoalem12 <lielmoalem12@gmail.com>
2023-04-27 10:34:16 +0300

    toggle dark mode

--- src/02-dark-mode/DarkMode.js
+++ src/02-dark-mode/DarkMode.js
@@ -1,8 +1,20 @@
-export default function DarkMode () {
+import { useState } from "react";
+
+export default function DarkMode() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode((prev) => !prev);
+  };
+
   return (
-    <div className='page'>
-      <button className='dark-mode-button'>Dark Mode</button>
-      <button className='light-mode-button'>Light Mode</button>
+    <div className={`page ${darkMode && "dark-mode"}`}>
+      <button className="dark-mode-button" onClick={toggleDarkMode}>
+        Dark Mode
+      </button>
+      <button className="light-mode-button" onClick={toggleDarkMode}>
+        Light Mode
+      </button>
     </div>
-  )
+  );
 }
